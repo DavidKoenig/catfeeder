d6574c753d12eb29702250271658744cabf76b89
DavidKoenig <david.koenig@fh-erfurt.de>
2016-06-06 16:09:29 +0200

    added python scripts and raspi-remote send file

---
+++ app/Resources/py/feed.py
@@ -0,0 +1,15 @@
+import time
+import subprocess
+import sys
+
+# arg[0] is the file name, so skip it
+channelCode = sys.argv[1]
+unitCode = sys.argv[2]
+duration = sys.argv[3]
+
+subprocess.call('sudo ./send ' + channelCode + ' ' + unitCode + ' 1', shell=True)
+time.sleep(float(duration))
+subprocess.call('sudo ./send ' + channelCode + ' ' + unitCode + ' 0', shell=True)
+
+    
+        
