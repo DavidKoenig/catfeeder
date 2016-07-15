1e230f5348dab60d64e0cda36cc1c3f7fddd605f
DavidKoenig <david.koenig@fh-erfurt.de>
2016-07-15 20:58:59 +0200

    move remote action stuff to feed controller; add form for button

--- src/AppBundle/Controller/FeedController.php
+++ src/AppBundle/Controller/FeedController.php
@@ -10,6 +10,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 
 class FeedController extends Controller
@@ -20,6 +21,44 @@ class FeedController extends Controller
      */
     public function feedAction(Request $request)
     {
-        return $this->render('AppBundle::feed.html.twig');
+        $formData = [];
+        $form = $this->createFormBuilder($formData, array(
+            'csrf_protection' => false,
+        ))
+        ->add('dummyField', TextType::class)
+        ->getForm();
+
+        if ($form->isSubmitted()) {
+            $source = $_SERVER['SERVER_ADDR'];
+            $target = shell_exec("hostname -I");
+            $port = 11337;
+
+            $nGroup = "11010";
+            $nSwitch = "04";
+            $nAction = "1";
+
+            $output = $nGroup.$nSwitch.$nAction;
+
+            $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("Could not create socket\n");
+            socket_bind($socket, $source) or die("Could not bind to socket\n");
+            socket_connect($socket, $target, $port) or die("Could not connect to socket\n");
+            socket_write($socket, $output, strlen ($output)) or die("Could not write output\n");
+            socket_close($socket);
+
+            $nAction = "0";
+
+            $output = $nGroup.$nSwitch.$nAction;
+            sleep(1);
+
+            $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("Could not create socket here\n");
+            socket_bind($socket, $source) or die("Could not bind to socket\n");
+            socket_connect($socket, $target, $port) or die("Could not connect to socket\n");
+            socket_write($socket, $output, strlen ($output)) or die("Could not write output\n");
+            socket_close($socket);
+        }
+
+        return $this->render('AppBundle::feed.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
--- src/AppBundle/Controller/SettingsController.php
+++ src/AppBundle/Controller/SettingsController.php
@@ -35,33 +35,6 @@ class SettingsController extends Controller
             $em->persist($settings);
             $em->flush();
             $isFormSaved = true;
-
-            $source = $_SERVER['SERVER_ADDR'];
-            $target = shell_exec("hostname -I");
-            $port = 11337;
-
-            $nGroup = "11010";
-            $nSwitch = "04";
-            $nAction = "1";
-
-            $output = $nGroup.$nSwitch.$nAction;
-
-            $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("Could not create socket here\n");
-            socket_bind($socket, $source) or die("Could not bind to socket\n");
-            socket_connect($socket, $target, $port) or die("Could not connect to socket\n");
-            socket_write($socket, $output, strlen ($output)) or die("Could not write output\n");
-            socket_close($socket);
-
-            $nAction = "0";
-
-            $output = $nGroup.$nSwitch.$nAction;
-            sleep(1);
-
-            $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("Could not create socket here\n");
-            socket_bind($socket, $source) or die("Could not bind to socket\n");
-            socket_connect($socket, $target, $port) or die("Could not connect to socket\n");
-            socket_write($socket, $output, strlen ($output)) or die("Could not write output\n");
-            socket_close($socket);
         }
 
         return $this->render('AppBundle::settings.html.twig', array(
