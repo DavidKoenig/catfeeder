6142cd2921235ca2b1de13d61957e4373618919a
DavidKoenig <david.koenig@fh-erfurt.de>
2016-07-20 18:51:50 +0200

    added params and kill process to script; added two forms

--- app/Resources/pi/catfeeder-sudo-script.sh
+++ app/Resources/pi/catfeeder-sudo-script.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 #
-# sudo script for allowing the user www-data to run commands with root privilegs.
+# params: 
+# $1 true or false --> activate or not
+# $2 channel code
+# $3 unit code
+# $4 time
+#
+### sudo script for allowing the user www-data to run commands with root privilegs. ###
 
-sudo python /var/www/html/cat-feeder/app/Resources/pi/light-barrier.py 11010 4 1 &
+### light barrier part ###
+# if true, then python script shall be activated, if it is false it should be killed and process id's should be given as arguments
+
+if [ $1 = "true" ]
+    then 
+        python /var/www/html/cat-feeder/app/Resources/pi/light-barrier.py $2 $3 $4 &
+fi
+
+if [ $1 = "false" ]
+    then
+        kill -9 `ps -ef | grep light-barrier.py | grep -v grep | awk '{print $2}'`
+fi
 
 exit 0
--- src/AppBundle/Controller/FeedController.php
+++ src/AppBundle/Controller/FeedController.php
@@ -12,6 +12,7 @@ use AppBundle\Entity\Settings;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -25,17 +26,40 @@ class FeedController extends Controller
     {
         // There has to be only one setting.
         $settings = $this->getDoctrine()->getRepository('AppBundle:Settings')->findAll()[0];
+        // Is the light-barrier on or off?
+        exec("ps -ef | grep light-barrier.py | grep -v grep | awk '{print$2}'", $outputLightBarrier);
+      	$lightBarrierActive = $outputLightBarrier ? true : false;
 
-        $formData = [];
-        $form = $this->createFormBuilder($formData, array(
+        // Forms
+        $feedFormData = [];
+        $feedForm = $this->get('form.factory')->createNamedBuilder('feed-form', FormType::class, $feedFormData, [
             'csrf_protection' => false,
-	        'allow_extra_fields' => true
+	    'allow_extra_fields' => true
+        ])
+        ->add('hiddenField', TextType::class, array('required' => false))
+        ->getForm();
+        $feedForm->handleRequest($request);
+
+        $barrierFormData = [];
+        $barrierForm = $this->get('form.factory')->createNamedBuilder('barrier-form', FormType::class	, $barrierFormData, array(
+            'csrf_protection' => false,
+	    'allow_extra_fields' => true
         ))
         ->add('hiddenField', TextType::class, array('required' => false))
         ->getForm();
-        $form->handleRequest($request);
+        $barrierForm->handleRequest($request);
 
-        if ($form->isSubmitted()) {
+	// Form submissions
+	if($barrierForm->isSubmitted()) {
+            if ($lightBarrierActive === true) {
+                dump(shell_exec('sudo /var/www/html/cat-feeder/app/Resources/pi/catfeeder-sudo-script.sh false &'));
+            } 
+            else {
+                dump(shell_exec('sudo /var/www/html/cat-feeder/app/Resources/pi/catfeeder-sudo-script.sh true 11010 4 1 &'));
+            }
+	}
+
+        if ($feedForm->isSubmitted()) {
             $source = $_SERVER['SERVER_ADDR'];
             $target = shell_exec("hostname -I");
             $port = 11337;
@@ -63,14 +87,12 @@ class FeedController extends Controller
             socket_connect($socket, $target, $port) or die("Could not connect to socket\n");
             socket_write($socket, $output, strlen ($output)) or die("Could not write output\n");
             socket_close($socket);
-
-            sleep(1);
-
-	    exec('sudo /var/www/html/cat-feeder/app/Resources/pi/catfeeder-sudo-script.sh', $outputScript, $returnVal);
         }
 
         return $this->render('AppBundle::feed.html.twig', array(
-            'form' => $form->createView(),
+            'lightBarrierActive' => $lightBarrierActive,
+            'feedForm' => $feedForm->createView(),
+	    'barrierForm' => $barrierForm->createView(),
         ));
     }
 }
